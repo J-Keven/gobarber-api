e9f04ed28e5a9af8cf45ad9f358b9066681894c1
J-keven <jhonasnunes425@gmail.com>
2020-08-26 23:22:58 -0300

    [feat]: add typeorm configs and migration for created table of appointments

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import logs from './middlewares/logsModdleware';
 import appoitmentsRoute from './routes/appointmentsRoutes';
 
+import './database/index';
+
 const app = express();
 app.use(express.json());
 app.use(logs);
