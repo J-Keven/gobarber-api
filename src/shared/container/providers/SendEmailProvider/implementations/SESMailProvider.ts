b95ae538300c07a0e1b2f1c0f2fbaaabf7558eea
J-keven <jhonasnunes425@gmail.com>
2020-10-27 23:02:45 -0300

    [feat]: add files of exmaples for .env and ormconfig

--- src/shared/container/providers/SendEmailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/SendEmailProvider/implementations/SESMailProvider.ts
@@ -20,7 +20,7 @@ class SESMailProvider implements ISendEmail {
     this.client = nodemailer.createTransport({
       SES: new aws.SES({
         apiVersion: '2010-12-01',
-        region: 'sa-east-1',
+        region: 'us-east-1',
       }),
     });
   }
